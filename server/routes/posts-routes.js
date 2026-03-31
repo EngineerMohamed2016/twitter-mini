15eeb9c5b7256e8f95401509930ab6a2000d91ee
Mohamed Ahmed <144080783+EngineerMohamed2016@users.noreply.github.com>
2026-03-31 10:13:50 +0200

    Update posts-routes.js

--- server/routes/posts-routes.js
+++ server/routes/posts-routes.js
@@ -4,18 +4,18 @@ import { addBookmark, getUserBookmarks, removeBookmark } from '../controllers/us
 
 const router = Router();
 
-router.get('/', getAllPosts); // feed page
+router.get('/', getAllPosts); 
 router.get('/recommended-posts',getRecommendedPosts );
 router.get('/bookmarks', getUserBookmarks);
 router.patch('/:postId/save-bookmark', addBookmark);
 router.patch('/:postId/unsave-bookmark', removeBookmark);
-router.get('/:id', getPost); // get post
+router.get('/:id', getPost); 
 router.get('/profile/:username/posts', getUserPosts);
 router.get('/profile/:username/replies', getUserReplies);
 router.get('/profile/:username/likes', getUserLikes);
-router.get('/:id/replies', getPostReplies); // get replies for each post
-router.post('/', createPost); // create post || reply
-router.delete('/:id', deletePost); // deltet post || reply
+router.get('/:id/replies', getPostReplies); 
+router.post('/', createPost); 
+router.delete('/:id', deletePost); 
 router.patch('/:id/like', addLike);
 router.patch('/:id/unlike', removeLike);
 
